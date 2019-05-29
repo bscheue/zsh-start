5475732f97a1d4f5c90fb8c16740c823a4aa28ab
Brian Scheuermann <bscheuer@andrew.cmu.edu>
2019-05-29 00:43:52 -0400

    working with tmux and recent directories now (basic)

--- start.plugin.zsh
+++ start.plugin.zsh
@@ -1,4 +1,5 @@
 echo "Pick an index from below:"
+echo ""
 echo "Recently visited directories"
 cat ~/.oh-my-zsh/custom/plugins/start/recent_dirs.txt
 
@@ -20,22 +21,23 @@ ord() {
 zmodload zsh/mapfile
 FNAME=~/.oh-my-zsh/custom/plugins/start/tmux_dirs.txt
 FLINES=( "${(f)mapfile[$FNAME]}" )
-LIST="${mapfile[$FNAME]}" # Not required unless stuff uses it
-integer POS=1             # Not required unless stuff uses it
-integer SIZE=$#FLINES     # Number of lines, not required unless stuff uses it
+integer POS=1
+integer SIZE=$#FLINES
 
 echo $sessions
 if [[ $sessions != 1 ]]
 then
-  echo "Running tmux sessions:"
-  # cat ~/.oh-my-zsh/custom/plugins/start/tmux_dirs.txt
-  for ITEM in $FLINES;
-  do
-      # Do stuff
-      echo `chr $((96 + $POS))`
-      echo $ITEM
+  if [ "$TERM" = "screen" ] && [ -n "$TMUX" ]
+  then
+    echo "Currently in a tmux session"
+  else
+    echo "Running tmux sessions:"
+    for ITEM in $FLINES;
+    do
+      printf "[%s] %s\n" $(chr $((96 + $POS))) $ITEM
       (( POS++ ))
-  done
+    done
+  fi
 else
   echo "No running tmux sessions"
 fi
@@ -45,14 +47,11 @@ read -r index
 if [[ $index =~ [0-9] ]]
 then
   cd "$(cat ~/.oh-my-zsh/custom/plugins/start/recent_dirs.txt | cut -c 3- | sed -n $(($index+1))p)"
-elif [[ $index =~ [a-z] ]]
+elif [[ $index =~ [a-z] ]] && [[ $(($(ord $index) - 97)) -lt $SIZE ]]
 then
-  # echo "${FLINES[1]}"
-  # echo $SIZE
-  # cd
   tmux attach -t "$($LIST[$(($(ord $index) - 96))] | cut -d \: -f $(($(ord $index) - 96)))"
 else
-  cd
+  echo "Nothing selected"
 fi
 
 zshexit () { dirs -lv > ~/.oh-my-zsh/custom/plugins/start/recent_dirs.txt; }
