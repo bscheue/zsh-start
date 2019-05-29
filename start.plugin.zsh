06605a8fad604d571b7c3e0eed5925ce2e819975
Brian Scheuermann <bscheuer@andrew.cmu.edu>
2019-05-29 15:34:29 -0400

    fix script for connecting to the selected tmux session

--- start.plugin.zsh
+++ start.plugin.zsh
@@ -5,6 +5,9 @@ echo "Recently visited directories:"
 tmux ls > ~/.oh-my-zsh/custom/plugins/start/tmux_dirs.txt 2> /dev/null
 sessions=$?
 
+# offset for one indexing into the alphabet using unicode
+ord_offset=96
+
 # attribution for chr and ord functions:
 # https://unix.stackexchange.com/questions/92447/bash-script-to-get-ascii-values-for-alphabet
 chr() {
@@ -45,7 +48,7 @@ then
     echo "Running tmux sessions:"
     for ITEM in $tmux_dirs_lines;
     do
-      printf "[%s] %s\n" $(chr $((96 + $IDXA))) $ITEM
+      printf "[%s] %s\n" $(chr $(($ord_offset + $IDXA))) $ITEM
       (( IDXA++ ))
     done
   fi
@@ -56,16 +59,19 @@ fi
 printf "\nSelect an option: "
 read -r index
 
+  echo $tmux_sessions_len
 if [[ $index =~ [0-9] ]] && [[ $index -lt recent_dirs_len ]]
 then
   printf "Going to directory [$(($index))]\n"
   cd "$(cat ~/.oh-my-zsh/custom/plugins/start/recent_dirs.txt | sed -n $(($index + 1))p)"
-elif [[ $index =~ [a-z] ]] && [[ $(($(ord $index) - 97)) -lt $tmux_sessions_len ]]
+elif [[ $index =~ [a-z] ]] && [[ $(($(ord $index) - $ord_offset)) -lt $tmux_sessions_len ]]
 then
   # still have bug connecting to tmux session
   # had only a and b open, but pressing c connected to b
-  printf "Connecting to tmux session [$(($(ord $index) - 96))]\n"
-  tmux attach -t "$($LIST[$(($(ord $index) - 96))] | cut -d \: -f $(($(ord $index) - 96)))"
+  printf "Connecting to tmux session [$(($(ord $index) - $ord_offset))]\n"
+  tmux attach -t $(cat ~/.oh-my-zsh/custom/plugins/start/tmux_dirs.txt |
+    sed -n $(($(ord $index) - $ord_offset))p |
+    cut -d \: -f -1)
 else
   echo "None selected"
 fi
